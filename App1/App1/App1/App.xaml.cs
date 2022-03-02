a3e4d1aea407ba7973c0070c202cd22b2c38f46c
hasancbngl <hasan.cobanoglu1@outlook.com.tr>
2022-03-02 20:25:05 +0300

    commit changes

--- App1/App1/App1/App.xaml.cs
+++ App1/App1/App1/App.xaml.cs
@@ -10,7 +10,7 @@ namespace App1
         {
             InitializeComponent();
 
-            MainPage = new GridPage();
+            MainPage = new RelativeLayoutPage();
         }
 
         protected override void OnStart()
